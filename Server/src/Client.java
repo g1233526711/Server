4bb19bb6ec516df1d119eaebee405cedd130a675
g1233526711 <gmotaebratt@gmail.com>
2023-07-31 07:54:24 -0400

    My deployment assignment

---
+++ Server/src/Client.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class Client 
+{
+   public static void main(String[] args) 
+   {
+    Scanner scan = new Scanner(System.in);
+    try
+    {
+      Socket s=new Socket("localhost",8080);
+      DataInputStream dis=new DataInputStream(s.getInputStream());
+      DataOutputStream dout=new DataOutputStream(s.getOutputStream());
+
+
+     System.out.print("\nEnter a Number : ");
+     int num = scan.nextInt();
+
+     dout.writeInt(num);
+     String ans = (String)dis.readUTF();
+     System.out.println("\nNumber "+num+" Is Prime Number: "+ans);
+     dout.flush();
+     dout.close();
+     s.close();
+    }
+     catch(Exception e)
+    {
+     System.out.println(e);
+    }
+     scan.close();
+  } 
+}
+
+
+
